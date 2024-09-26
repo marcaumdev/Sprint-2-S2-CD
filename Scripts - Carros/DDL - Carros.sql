1e67652bc7105e606b9465ada21838135e7fd2ed
Marcos Vinícius Oliveira Dos Santos <48544763898@info.local>
2024-09-25 22:19:48 -0300

    bd carros

---
+++ Scripts - Carros/DDL - Carros.sql
@@ -0,0 +1,42 @@
+-- CRIAR BASE DE DADOS (DATABASE)
+
+CREATE DATABASE bdCarro;
+
+-- ACESSAR A BASE DE DADOS
+USE bdCarro;
+
+-- CRIAR TABELA
+CREATE TABLE tbCarros(
+id INT(10) NOT NULL,
+marca VARCHAR(100) NOT NULL,
+modelo VARCHAR(100) NOT NULL,
+valor DECIMAL(10,2) NOT NULL,
+cor VARCHAR(50) NOT NULL,
+numeroVendas INT(10),
+ano INT(4),
+PRIMARY KEY(id)
+);
+
+-- NOT NULL -> Campo Obrigatório
+
+SELECT * FROM tbCarros;
+
+CREATE TABLE tbProprietario(
+id INT(10) NOT NULL,
+nome VARCHAR(100) NOT NULL,
+idCarro INT(10) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (idCarro) REFERENCES tbCarros(id)
+);
+
+CREATE TABLE tbHistoricoPreco(
+id INT(10) NOT NULL AUTO_INCREMENT,
+dataModificacao DATETIME,
+valorAnterior DECIMAL(10,2),
+valorNovo DECIMAL(10,2),
+idCarro INT(10) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (idCarro) REFERENCES tbCarros(id)
+);
+-- DELETAR A BASE
+-- DROP DATABASE bd_carro;
---
+++ Scripts - Carros/DML - Carros.sql
@@ -0,0 +1,9 @@
+INSERT INTO tbcarros VALUES(
+'1',
+'Fiat',
+'Uno',
+'20000.00',
+'Verde',
+'50000',
+'1990'
+);
